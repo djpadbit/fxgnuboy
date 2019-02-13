8d9ae26cb25ca51fc275eef75e9e62bd72bfa622
djpadbit <djpadbit@gmail.com>
2019-02-13 17:38:27 +0100

    Don't use malloc for the file path
    Signed-off-by: djpadbit <djpadbit@gmail.com>

--- gnuboy-fx/rombank.c
+++ gnuboy-fx/rombank.c
@@ -113,14 +113,12 @@ uint8_t *getRomBank(int bank) {
 // Based on EDIT's source code (see FilePath function)
 void file_make_path(uint16_t* dst,char* root,char *fold,char *fn)
 {
-	char *tp;
-	tp = (char*)malloc(2+strlen(root)+1+strlen(fold)+1+strlen(fn)+1); // probably off by 1 or 2 bytes
+	char tp[2+strlen(root)+1+strlen(fold)+1+strlen(fn)+1]; // probably off by 1 or 2 bytes
 	if(strlen(fold)==0) sprintf(tp,"\\\\%s\\%s",root,fn); //File path without folder
 	else if(strlen(fn)==0) sprintf(tp,"\\\\%s\\%s",root,fn); //File path without file
 	else sprintf(tp,"\\\\%s\\%s\\%s",root,fold,fn); //File path with file & folder
 	for (int i=0;i<strlen(tp);i++) dst[i] = tp[i];
 	dst[strlen(tp)] = 0;
-	free(tp);
 }
 
 int rombankLoad(char *rom) {
